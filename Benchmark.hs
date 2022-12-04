b1f498e2cbb88d71412aacde2a7271377f73fde2
Michalis Pardalos <m.pardalos@gmail.com>
2022-12-04 22:28:52 +0000

    Use Criterion.env for loading inputs in benchmark

--- Benchmark.hs
+++ Benchmark.hs
@@ -2,20 +2,10 @@ module Main where
 
 import           Solutions
 import           Criterion.Main
-import qualified Data.Map.Strict as Map
-import           Data.Map.Strict(Map, (!))
-import           System.Directory
-import           Control.Monad (forM)
 
 main :: IO ()
-main = do
-  inputs <- do
-    filenames <- listDirectory "data/"
-    list <- forM filenames $ \filename -> do
-      contents <- readFile ("data/" <> filename)
-      return (filename, contents)
-    return (Map.fromList list)
-
-  defaultMain
-    $ map (\(MkSolution name solution inputName) -> bench name $ whnf solution (inputs ! inputName))
-    $ solutions
+main = defaultMain
+  [ env (readFile ("data/" <> inputName)) $ \input ->
+      bench name (whnf solution input)
+  | MkSolution name solution inputName <- solutions
+  ]
