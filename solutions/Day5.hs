0157d744166b723262106d1d90e9aab867170e80
Michalis Pardalos <m.pardalos@gmail.com>
2022-12-05 18:05:01 +0000

    Solve day 5

--- solutions/Day5.hs
+++ solutions/Day5.hs
@@ -1,13 +1,14 @@
 {-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module Day5 (part1, part2) where
 
 import Data.ByteString (ByteString)
 import Data.ByteString.Char8 qualified as BS
+import Data.Foldable (foldl')
 import Data.Vector (Vector)
 import Data.Vector qualified as V
-import Debug.Trace
 
 type State = Vector ByteString
 
@@ -44,29 +45,45 @@ readState fullInput = go (V.replicate numColumns BS.empty) fullInput
            in go (readStateLine state line) (BS.drop 1 rest)
 
 data Move = Move
-  { count :: Int
-  , from :: Int
-  , to :: Int
+  { count :: Int,
+    from :: Int,
+    to :: Int
   }
-  deriving Show
+  deriving (Show)
 
 readMove :: ByteString -> Move
 readMove line =
   let s1 = BS.drop 5 line
       Just (count, s2) = BS.readInt s1
       s3 = BS.drop 6 s2
-      Just (from, s4) = BS.readInt s3
+      Just (fromRead, s4) = BS.readInt s3
       s5 = BS.drop 4 s4
-      Just (to, _) = BS.readInt s5
-   in Move {..}
+      Just (toRead, _) = BS.readInt s5
+   in Move {count, from = fromRead - 1, to = toRead - 1}
 
+applyMove :: Bool -> State -> Move -> State
+applyMove shouldReverse state Move {..} =
+  let crates = BS.take count $ state V.! from
+   in V.imap
+        ( \i stk ->
+            if
+                | i == from -> BS.drop count stk
+                | i == to && shouldReverse -> BS.reverse crates <> stk
+                | i == to -> crates <> stk
+                | otherwise -> stk
+        )
+        state
 
-part1 :: ByteString -> Int
-part1 input =
+solve :: Bool -> ByteString -> String
+solve shouldReverse input =
   let (state, rest) = readState input
       moveLines = drop 2 (BS.lines rest)
       moves = map readMove moveLines
-   in traceShow (take 2 moves) (-1)
+      finalState = foldl' (applyMove shouldReverse) state moves
+   in V.toList $ V.map BS.head finalState
 
-part2 :: ByteString -> Int
-part2 _ = -1
+part1 :: ByteString -> String
+part1 = solve True
+
+part2 :: ByteString -> String
+part2 = solve False
