cd017efdc5694417a387988f0b5d0eb8dddeda9e
Michalis Pardalos <m.pardalos@gmail.com>
2022-12-05 11:15:17 +0000

    Fancy box for output

--- Main.hs
+++ Main.hs
@@ -1,12 +1,28 @@
-{-# OPTIONS -fdefer-typed-holes #-}
+{-# OPTIONS_GHC -Wno-typed-holes #-}
 module Main where
 
 import           Solutions
 import           Text.Printf   (printf)
 import           Control.Monad (forM_)
+import           Data.Foldable
+
+titleLength :: Int
+titleLength = maximum [ length name
+                      | MkSolution name _ _ <- solutions
+                      ]
+
+printTableAnchor :: Bool -> IO ()
+printTableAnchor top = do
+  putStr (if top then "┌" else "└")
+  putStrLn $ take (15 + titleLength) $ repeat '─'
+
+printLine :: String -> String -> IO ()
+printLine = printf "│ %*s │ %s \n" titleLength
 
 main :: IO ()
 main = do
+  printTableAnchor True
   forM_ solutions $ \(MkSolution name solution inputFile) -> do
     input <- readFile ("data/" <> inputFile)
-    printf "%s: %s\n" name (show $ solution input)
+    printLine name (show $ solution input)
+  printTableAnchor False
