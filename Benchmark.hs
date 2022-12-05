3cfe4d05c993707e72e636b72b82191a2afab38e
Michalis Pardalos <m.pardalos@gmail.com>
2022-12-05 10:53:32 +0000

    Generate benchmark report by default

--- Benchmark.hs
+++ Benchmark.hs
@@ -2,9 +2,11 @@ module Main where
 
 import           Solutions
 import           Criterion.Main
+import           Criterion.Types (Config(..))
 
 main :: IO ()
-main = defaultMain
+main = defaultMainWith
+  defaultConfig { reportFile = Just "benchmark.html" }
   [ env (readFile ("data/" <> inputName)) $ \input ->
       bench name (whnf solution input)
   | MkSolution name solution inputName <- solutions
