d0bca14b1b3c378b046bda93922fa8b1300b0a77
Michalis Pardalos <m.pardalos@gmail.com>
2022-12-07 15:10:37 +0000

    Improve day 4 performance using strictness + inlining

--- solutions/Day4.hs
+++ solutions/Day4.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+{-# LANGUAGE BangPatterns #-}
 
 module Day4 (part1, part2) where
 
@@ -7,14 +8,16 @@ import Data.ByteString.Char8 qualified as BS
 
 readSectionAssignment :: ByteString -> ((Int, Int), (Int, Int))
 readSectionAssignment s =
-  let Just (a, s1) = BS.readInt s
-      s2 = BS.drop 1 s1
-      Just (b, s3) = BS.readInt s2
-      s4 = BS.drop 1 s3
-      Just (c, s5) = BS.readInt s4
-      s6 = BS.drop 1 s5
-      Just (d, _s7) = BS.readInt s6
+  let Just !(a, s1) = BS.readInt s
+      !s2 = BS.drop 1 s1
+      Just !(b, s3) = BS.readInt s2
+      !s4 = BS.drop 1 s3
+      Just !(c, s5) = BS.readInt s4
+      !s6 = BS.drop 1 s5
+      Just !(d, _s7) = BS.readInt s6
    in ((a, b), (c, d))
+
+{-# INLINE readSectionAssignment #-}
 
 part1 :: ByteString -> Int
 part1 =
