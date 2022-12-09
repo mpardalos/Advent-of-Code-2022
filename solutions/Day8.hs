ad3cb74e37ac7abeb4792f6a722a06b6a7eed694
Michalis Pardalos <m.pardalos@gmail.com>
2022-12-09 00:07:55 +0000

    Solve day 8 part 2

--- solutions/Day8.hs
+++ solutions/Day8.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TupleSections #-}
 
 module Day8 (part1, part2) where
@@ -17,23 +18,33 @@ import Data.Vector.Mutable (MVector)
 import Data.Vector.Mutable qualified as MV
 import Debug.Trace
 import GHC.Stack (HasCallStack)
+import Text.Printf (printf)
 
-readInput :: ByteString -> (Int, Vector (Bool, Int))
-readInput input = (length rows, forest)
+readInput :: ByteString -> (Int, Vector Int)
+readInput input = (length rows, V.fromList $ concat rows)
   where
-    forest = V.fromList $ map (False,) $ concat rows
-    rows = map (map ord . BS.unpack) $ BS.lines input
+    rows = map (map readDigit . BS.unpack) $ BS.lines input
+    readDigit '0' = 0
+    readDigit '1' = 1
+    readDigit '2' = 2
+    readDigit '3' = 3
+    readDigit '4' = 4
+    readDigit '5' = 5
+    readDigit '6' = 6
+    readDigit '7' = 7
+    readDigit '8' = 8
+    readDigit '9' = 9
 
 part1 :: HasCallStack => ByteString -> Int
 part1 input = length $ filter fst $ V.toList visibleForest
   where
     (size, initialForest) = readInput input
-    visibleForest = V.modify markVisible initialForest
+    visibleForest = V.modify markExternallyVisible $ V.map (False,) initialForest
 
     index row col = size * row + col
 
-    markVisiblePass :: forall s. STRef s Int -> MVector s (Bool, Int) -> Int -> ST s ()
-    markVisiblePass maxSeenRef forest idx =
+    markExternallyVisiblePass :: forall s. STRef s Int -> MVector s (Bool, Int) -> Int -> ST s ()
+    markExternallyVisiblePass maxSeenRef forest idx =
       MV.modifyM
         forest
         ( \(alreadyVisible, height) -> do
@@ -46,39 +57,119 @@ part1 input = length $ filter fst $ V.toList visibleForest
         )
         idx
 
-    markVisible :: forall s. MVector s (Bool, Int) -> ST s ()
-    markVisible forest = do
+    markExternallyVisible :: forall s. MVector s (Bool, Int) -> ST s ()
+    markExternallyVisible forest = do
       -- Rows, left-to-right
       forM_ [0 .. size - 1] $ \row -> do
         maxSeenRef <- newSTRef 0
         forM_ [0 .. size - 1] $ \col ->
-          markVisiblePass maxSeenRef forest (index row col)
+          markExternallyVisiblePass maxSeenRef forest (index row col)
 
       -- Rows, right-to-left
       forM_ [0 .. size - 1] $ \row -> do
         maxSeenRef <- newSTRef 0
         forM_ (reverse [0 .. size - 1]) $ \col ->
-          markVisiblePass maxSeenRef forest (index row col)
+          markExternallyVisiblePass maxSeenRef forest (index row col)
 
       -- Columns, top-to-bottom
       forM_ [0 .. size - 1] $ \col -> do
         maxSeenRef <- newSTRef 0
         forM_ [0 .. size - 1] $ \row ->
-          markVisiblePass maxSeenRef forest (index row col)
+          markExternallyVisiblePass maxSeenRef forest (index row col)
 
       -- Columns, bottom-to-top
       forM_ [0 .. size - 1] $ \col -> do
         maxSeenRef <- newSTRef 0
         forM_ (reverse [0 .. size - 1]) $ \row ->
-          markVisiblePass maxSeenRef forest (index row col)
+          markExternallyVisiblePass maxSeenRef forest (index row col)
 
-part2 :: ByteString -> Int
-part2 = const 0
+data ScenicScores = ScenicScores
+  { west :: Int,
+    north :: Int,
+    east :: Int,
+    south :: Int
+  }
 
--- visibleTreesInLine :: Foldable t => t Int -> Int
--- visibleTreesInLine = fst . foldl' go (0, 0)
---   where
---     go :: (Int, Int) -> Int -> (Int, Int)
---     go (count, maxSeen) tree
---       | tree > maxSeen = (count + 1, tree)
---       | otherwise = (count, maxSeen)
+instance Show ScenicScores where
+  show (ScenicScores w n e s) = printf "%2d*%2d*%2d*%2d" w n e s
+
+summarizeScore :: ScenicScores -> Int
+summarizeScore (ScenicScores w n e s) = w * n * e * s
+
+maxHeight :: Int
+maxHeight = 10
+
+showForest :: Show a => Int -> Vector (a, Int) -> String
+showForest size forest =
+  let (row, rest) = V.splitAt size forest
+   in if V.length row > 0
+        then "| " ++ show row ++ " |\n" ++ showForest size rest
+        else ""
+
+part2 :: HasCallStack => ByteString -> Int
+part2 input = maximum $ map (summarizeScore . fst) $ V.toList scenicForest
+  where
+    (size, initialForest) = readInput input
+    scenicForest = V.modify countVisible $ V.map (ScenicScores 0 0 0 0,) initialForest
+
+    index row col = size * row + col
+
+    -- Keep track of the index at which a height was last seen. Each tree can
+    -- see back to the index of the closest tree equal to or taller than itself
+    countVisible :: forall s. MVector s (ScenicScores, Int) -> ST s ()
+    countVisible forest = do
+      -- Rows, left-to-right
+      forM_ [0 .. size - 1] $ \row -> do
+        lastSeenVec <- MV.replicate maxHeight 0
+        forM_ [0 .. size - 1] $ \col ->
+          MV.modifyM
+            forest
+            ( \(scenicScores, height) -> do
+                closestBlockingIndex <- maximum <$> V.freeze (MV.drop height lastSeenVec)
+                let directionalScenicScore = col - closestBlockingIndex
+                MV.write lastSeenVec height col
+                return (scenicScores {west = directionalScenicScore}, height)
+            )
+            (index row col)
+
+      -- Rows, right-to-left
+      forM_ [0 .. size - 1] $ \row -> do
+        lastSeenVec <- MV.replicate maxHeight (size - 1)
+        forM_ (reverse [0 .. size - 1]) $ \col ->
+          MV.modifyM
+            forest
+            ( \(scenicScores, height) -> do
+                closestBlockingIndex <- minimum <$> V.freeze (MV.drop height lastSeenVec)
+                let directionalScenicScore = closestBlockingIndex - col
+                MV.write lastSeenVec height col
+                return (scenicScores {east = directionalScenicScore}, height)
+            )
+            (index row col)
+
+      -- Columns, top-to-bottom
+      forM_ [0 .. size - 1] $ \col -> do
+        lastSeenVec <- MV.replicate maxHeight 0
+        forM_ [0 .. size - 1] $ \row ->
+          MV.modifyM
+            forest
+            ( \(scenicScores, height) -> do
+                closestBlockingIndex <- maximum <$> V.freeze (MV.drop height lastSeenVec)
+                let directionalScenicScore = row - closestBlockingIndex
+                MV.write lastSeenVec height row
+                return (scenicScores {north = directionalScenicScore}, height)
+            )
+            (index row col)
+
+      -- Columns, bottom-to-top
+      forM_ [0 .. size - 1] $ \col -> do
+        lastSeenVec <- MV.replicate 10 (size - 1)
+        forM_ (reverse [0 .. size - 1]) $ \row ->
+          MV.modifyM
+            forest
+            ( \(scenicScores, height) -> do
+                closestBlockingIndex <- minimum <$> V.freeze (MV.drop height lastSeenVec)
+                let directionalScenicScore = closestBlockingIndex - row
+                MV.write lastSeenVec height row
+                return (scenicScores {south = directionalScenicScore}, height)
+            )
+            (index row col)
