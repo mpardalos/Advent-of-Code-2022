b310f652042a3a5465d2550334b1c6efd0758b7e
Michalis Pardalos <m.pardalos@gmail.com>
2022-12-09 22:12:59 +0000

    Use an IntSet in day9 to count positions

--- solutions/Day9.hs
+++ solutions/Day9.hs
@@ -1,12 +1,15 @@
 module Day9 (part1, part2) where
 
+import Data.Bits (shiftL, (.&.), (.|.))
 import Data.ByteString (ByteString)
 import Data.ByteString.Char8 qualified as BS
+import Data.Int (Int32)
+import Data.IntSet qualified as IntSet
 import Data.List (scanl')
 import Data.Maybe (fromJust)
-import Data.Set qualified as Set
+import Debug.Trace (traceShow)
 
-data Position = P {-# UNPACK #-} !Int {-# UNPACK #-} !Int deriving (Eq, Ord, Show)
+data Position = P {-# UNPACK #-} !Int32 {-# UNPACK #-} !Int32 deriving (Eq, Ord, Show)
 
 readInput :: ByteString -> [Position -> Position]
 readInput =
@@ -30,25 +33,34 @@ tailFollow (P tailX tailY) (P headX headY) =
         then P tailX tailY
         else P (tailX + sign xdiff) (tailY + sign ydiff)
 
+countUniquePositions :: [Position] -> Int
+countUniquePositions =
+  IntSet.size
+    . IntSet.fromList
+    -- We combine a position into a single integer. This allows us to use IntSet
+    -- for counting unique positions (which is much faster than Set).
+    -- To do this, we assume that each component of the position only needs 16
+    -- bits, and so we pack the two componenents into a single 32 bit int
+    . map (\(P x y) -> fromIntegral $ shiftL x 16 .|. (y .&. 0xFFFF))
+
 part1 :: ByteString -> Int
 part1 =
-  Set.size
-    . Set.fromList
+  countUniquePositions
     . scanl1 tailFollow
     . scanl' (flip ($)) (P 0 0)
     . readInput
 
 part2 :: ByteString -> Int
 part2 =
-  Set.size
-    . Set.fromList
+  countUniquePositions
     . (!! 9)
     . iterate (scanl1 tailFollow)
     . scanl' (flip ($)) (P 0 0)
     . readInput
 
-sign :: Int -> Int
+sign :: (Ord a, Num a, Num b) => a -> b
 sign x
   | x < 0 = -1
   | x > 0 = 1
   | otherwise = 0
+{-# INLINE sign #-}
