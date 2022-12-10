a38f60dcfa65fec73a76f4e3cd965da4cb784f35
Michalis Pardalos <m.pardalos@gmail.com>
2022-12-10 14:48:41 +0000

    Extract some parts into functions for day 10

--- solutions/Day10.hs
+++ solutions/Day10.hs
@@ -2,7 +2,7 @@ module Day10 (part1, part2) where
 
 import Data.ByteString (ByteString)
 import Data.ByteString.Char8 qualified as BS
-import Data.List (intercalate, scanl')
+import Data.List (scanl')
 import Data.List.Split (chunksOf)
 import Data.Maybe (fromJust)
 
@@ -17,31 +17,31 @@ readInputLine line = case BS.head line of
   'n' -> [id]
   _ -> error ("Invalid input line: " <> BS.unpack line)
 
+registerValuesFromInput :: ByteString -> [Int]
+registerValuesFromInput =
+  scanl' runInstruction 1
+    . concatMap readInputLine
+    . BS.lines
+
 part1 :: ByteString -> Int
 part1 input =
-  sum $
-    [ n * registerValues !! (n - 1)
+  sum
+    [ n * registerValuesFromInput input !! (n - 1)
       | n <- [20, 60, 100, 140, 180, 220]
     ]
-  where
-    registerValues =
-      scanl' runInstruction 1 $
-        concatMap readInputLine $
-          BS.lines input
+
+renderRow :: [Int] -> [Char]
+renderRow rowSpritePositions =
+  [ if abs (spritePosition - pixelRowIndex) <= 1
+      then '█'
+      else ' '
+    | (pixelRowIndex, spritePosition) <- zip [0 ..] rowSpritePositions
+  ]
 
 part2 :: ByteString -> String
-part2 input = intercalate "\n" $ map renderRow $ take 6 $ chunksOf 40 spritePositions
-  where
-    renderRow :: [Int] -> [Char]
-    renderRow rowSpritePositions =
-      [ if abs (spritePosition - rowPixelIndex) <= 1
-          then '█'
-          else ' '
-        | (rowPixelIndex, spritePosition) <- zip [0 ..] rowSpritePositions
-      ]
-
-    spritePositions :: [Int]
-    spritePositions =
-      scanl' runInstruction 1 $
-        concatMap readInputLine $
-          BS.lines input
+part2 =
+  unlines
+    . map renderRow
+    . take 6
+    . chunksOf 40
+    . registerValuesFromInput
