bb9e2eba9960dda71f660f01f63ddf72c5063e1d
Michalis Pardalos <m.pardalos@gmail.com>
2022-12-11 23:52:16 +0000

    Implement day 11 part 2 (correctly)

--- solutions/Day11.hs
+++ solutions/Day11.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Avoid lambda using `infix`" #-}
 
 module Day11 (part1, part2) where
 
@@ -17,6 +20,7 @@ import Data.Vector.Generic.Mutable qualified as MV
 import Data.Vector.Mutable (MVector)
 import GHC.Stack (HasCallStack)
 import Text.Printf (printf)
+import Debug.Trace
 
 type MonkeyId = Int
 
@@ -27,6 +31,7 @@ data Monkey = Monkey
     items :: Vector WorryValue,
     operation :: WorryValue -> WorryValue,
     test :: WorryValue -> MonkeyId,
+    testModulo :: Integer,
     itemsInspected :: Int
   }
 
@@ -52,20 +57,21 @@ readMonkey
       { number = readIntPartial $ BS.drop 7 numLine,
         items = V.fromList $ map (fromIntegral . readIntPartial) $ BS.splitWith (== ' ') $ BS.drop 18 itemsLine,
         operation = operation,
-        test = \n -> if n `mod` divisor == 0 then ifTrueTarget else ifFalseTarget,
+        test = \n -> if n `mod` testModulo == 0 then ifTrueTarget else ifFalseTarget,
+        testModulo = testModulo,
         itemsInspected = 0
       }
     where
-      operationArithmetic = case operationLine `BS.index` 23 of
-        '*' -> (*)
-        '+' -> (+)
-        c -> error ("Unknown operation: " <> show c)
+      operation = case (operationLine `BS.index` 23, operationLine `BS.index` 25) of
+        ('*', 'o') -> \n -> n * n
+        ('+', 'o') -> \n -> n + n
+        ('*', _) -> \n -> n * operand
+        ('+', _) -> \n -> n + operand
+        _ -> error "Invalid input"
 
-      operation = case operationLine `BS.index` 25 of
-        'o' -> \n -> n `operationArithmetic` n
-        _ -> \n -> n `operationArithmetic` fromIntegral (readIntPartial (BS.drop 25 operationLine))
-
-      divisor = fromIntegral $ readIntPartial $ BS.drop 21 testLine
+      -- should only be used if the second operand is not 'old'
+      operand = fromIntegral (readIntPartial (BS.drop 25 operationLine))
+      testModulo = fromIntegral $ readIntPartial $ BS.drop 21 testLine
       ifTrueTarget = fromIntegral $ readIntPartial $ BS.drop 29 ifTrueLine
       ifFalseTarget = fromIntegral $ readIntPartial $ BS.drop 30 ifFalseLine
 readMonkey _ = error "Invalid input"
@@ -79,11 +85,13 @@ runMonkeyPart1 Monkey {items, operation, test} =
     )
     items
 
-runMonkeyPart2 :: Monkey -> Vector (MonkeyId, WorryValue)
-runMonkeyPart2 Monkey {items, operation, test} =
+-- The critical insight here is that all arithmetic can be done modulo the
+-- product of all the moduli used for the monkeys' tests
+runMonkeyPart2 :: Integer -> Monkey -> Vector (MonkeyId, WorryValue)
+runMonkeyPart2 modulo Monkey {items, operation, test} =
   V.map
     ( \worryVal ->
-        let newWorryVal = operation worryVal
+        let newWorryVal = operation worryVal `mod` modulo
          in (test newWorryVal, newWorryVal)
     )
     items
@@ -111,8 +119,7 @@ runRound runMonkey monkeys = do
         toMonkey
 
 part1 :: ByteString -> Int
-part1 input =
-  product $ take 2 $ reverse $ sort $ V.toList $ V.map itemsInspected finalMonkeys
+part1 input = product $ take 2 $ reverse $ sort $ V.toList $ V.map itemsInspected finalMonkeys
   where
     initialMonkeys = readInput input
     finalMonkeys = runST $ do
@@ -124,7 +131,9 @@ part2 :: ByteString -> Int
 part2 input = product $ take 2 $ reverse $ sort $ V.toList $ V.map itemsInspected finalMonkeys
   where
     initialMonkeys = readInput input
+    modulo = product $ map testModulo $ V.toList initialMonkeys
+
     finalMonkeys = runST $ do
       monkeysMut <- V.thaw initialMonkeys
-      replicateM_ 10000 (runRound runMonkeyPart2 monkeysMut)
+      replicateM_ 10000 (runRound (runMonkeyPart2 modulo) monkeysMut)
       V.freeze monkeysMut
