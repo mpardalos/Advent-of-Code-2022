5c1d0d20dc7263449208b245f437c1b4a9afb336
Michalis Pardalos <me@mpardalos.com>
2022-12-13 12:09:39 +0000

    Day 12 - Separate heights and distances arrays

--- solutions/Day12.hs
+++ solutions/Day12.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE LambdaCase #-}
@@ -7,7 +8,7 @@ module Day12 (part1, part2) where
 
 import Control.DeepSeq (NFData)
 import Control.Monad (forM_, when)
-import Control.Monad.ST (ST)
+import Control.Monad.ST (ST, runST)
 import Data.ByteString (ByteString)
 import Data.ByteString.Char8 qualified as BS
 import Data.Char (chr, ord)
@@ -26,7 +27,7 @@ type Coordinates = M.Ix2
 data Tile
   = Start
   | End
-  | Height Int
+  | Height {-# UNPACK #-} !Int
   deriving (Eq, Generic, NFData)
 
 instance Show Tile where
@@ -58,36 +59,35 @@ findEnd :: Grid Tile -> Coordinates
 findEnd = fromJustNote "No start coordinates" . M.findIndex (== End)
 
 -- | Set the distances to every position on the grid from a starting position
-findDistancesST :: (Int -> Int -> Bool) -> Coordinates -> Int -> MGrid s (Maybe Int, Tile) -> ST s ()
-findDistancesST canReach c distanceCovered g = do
-  (mBestDistance, currentPos) <- M.readM g c
+findDistancesST :: (Int -> Int -> Bool) -> Coordinates -> Int -> Grid Tile -> MGrid s Int -> ST s ()
+findDistancesST canReach c !distanceCovered heights distances = do
+  bestDistance <- M.readM distances c
 
-  let shouldUpdate = case mBestDistance of
-        (Just bestDistance) -> distanceCovered < bestDistance
-        Nothing -> True
-
+  let shouldUpdate = bestDistance < 0 || distanceCovered < bestDistance
   when shouldUpdate $ do
-    M.modifyM_ g (\(_, p) -> pure (Just distanceCovered, p)) c
+    M.write_ distances c distanceCovered
     forM_ [c + (0 :. -1), c + (-1 :. 0), c + (0 :. 1), c + (1 :. 0)] $ \c' -> do
-      M.read g c' >>= \case
+      case heights M.!? c' of
         Nothing -> pure ()
-        Just (_, nextPos) ->
-          when (canReach (height currentPos) (height nextPos)) $
-            findDistancesST canReach c' (distanceCovered + 1) g
+        Just !nextHeight -> do
+          let currentHeight = heights M.! c
+          if canReach (height currentHeight) (height nextHeight)
+            then findDistancesST canReach c' (distanceCovered + 1) heights distances
+            else pure ()
 
-findDistances :: (Int -> Int -> Bool) -> Coordinates -> Int -> Grid (Maybe Int, Tile) -> Grid (Maybe Int, Tile)
-findDistances canReach c distanceCovered g = M.withMArrayST_ g (findDistancesST canReach c distanceCovered)
+findDistances :: (Int -> Int -> Bool) -> Coordinates -> Int -> Grid Tile -> Grid Int
+findDistances canReach c distanceCovered heights = runST $ do
+  distances <- M.newMArray (M.size heights) (-1)
+  findDistancesST canReach c distanceCovered heights distances
+  M.freezeS distances
 
 part1 :: ByteString -> Int
 part1 input =
   inputGrid
-    & M.map (Nothing,)
     & M.compute
     & findDistances (\from to -> to <= from + 1) startCoords 0
     & (M.!? endCoords)
     & fromJustNote "End is out of bounds"
-    & fst
-    & fromJustNote "End not reached"
   where
     startCoords = findStart inputGrid
     endCoords = findEnd inputGrid
@@ -96,12 +96,12 @@ part1 input =
 part2 :: ByteString -> Int
 part2 input =
   inputGrid
-    & M.map (Nothing,)
     & M.compute
     & findDistances (\from to -> to >= from - 1) endCoords 0
+    & M.zip inputGrid
     & M.flatten
-    & M.sfilter (\(_, pos) -> height pos == ord 'a')
-    & M.smapMaybe fst
+    & M.sfilter (\(tile, _) -> height tile == ord 'a')
+    & M.smapMaybe (\(_, distance) -> if distance >= 0 then Just distance else Nothing)
     & M.sminimum'
   where
     inputGrid = readInput input
