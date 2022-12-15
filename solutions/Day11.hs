79504b768e8c1de540bd5b4d4d3917c49ce95c96
Michalis Pardalos <me@mpardalos.com>
2022-12-15 01:07:45 +0000

    Use massiv for day 11

--- solutions/Day11.hs
+++ solutions/Day11.hs
@@ -1,25 +1,22 @@
+{-# HLINT ignore "Avoid lambda using `infix`" #-}
+{-# OPTIONS -fprof-auto #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
-{-# HLINT ignore "Avoid lambda using `infix`" #-}
-{-# OPTIONS -fprof-auto #-}
-
 module Day11 (part1, part2) where
 
-import Control.Monad (replicateM_)
-import Control.Monad.ST (ST, runST)
 import Data.ByteString (ByteString)
 import Data.ByteString.Char8 qualified as BS
-import Data.Functor.Identity (Identity (..))
+import Data.Function ((&))
+import Data.IntMap (IntMap)
 import Data.List (iterate', sort)
 import Data.List.Split
+import Data.Massiv.Array (Vector)
+import Data.Massiv.Array qualified as M
 import Data.Maybe (fromJust)
-import Data.STRef
-import Data.Vector (Vector)
-import Data.Vector qualified as V
-import Debug.Trace
 
 type MonkeyId = Int
 
@@ -27,15 +24,15 @@ type WorryValue = Int
 
 data Monkey = Monkey
   { number :: MonkeyId,
-    items :: Vector WorryValue,
+    items :: Vector M.P WorryValue,
     operation :: WorryValue -> WorryValue,
     test :: WorryValue -> MonkeyId,
     testModulo :: Int,
     itemsInspected :: Int
   }
 
-readInput :: ByteString -> Vector Monkey
-readInput = V.fromList . map readMonkey . splitOn [""] . BS.lines
+readInput :: ByteString -> Vector M.B Monkey
+readInput = M.fromList M.Par . map readMonkey . splitOn [""] . BS.lines
 
 readIntPartial :: ByteString -> Int
 readIntPartial = fst . fromJust . BS.readInt
@@ -51,7 +48,7 @@ readMonkey
     ] =
     Monkey
       { number = readIntPartial $ BS.drop 7 numLine,
-        items = V.fromList $ map (fromIntegral . readIntPartial) $ BS.splitWith (== ' ') $ BS.drop 18 itemsLine,
+        items = M.fromList M.Par $ map (fromIntegral . readIntPartial) $ BS.splitWith (== ' ') $ BS.drop 18 itemsLine,
         operation = operation,
         test = \n -> if n `mod` testModulo == 0 then ifTrueTarget else ifFalseTarget,
         testModulo = testModulo,
@@ -72,50 +69,65 @@ readMonkey
       ifFalseTarget = fromIntegral $ readIntPartial $ BS.drop 30 ifFalseLine
 readMonkey _ = error "Invalid input"
 
-runMonkeyPart1 :: Monkey -> Vector (MonkeyId, WorryValue)
+runMonkeyPart1 :: Monkey -> Vector M.U (MonkeyId, WorryValue)
 runMonkeyPart1 Monkey {items, operation, test} =
-  V.map
-    ( \worryVal ->
-        let newWorryVal = operation worryVal `div` 3
-         in (test newWorryVal, newWorryVal)
-    )
-    items
+  M.compute $
+    M.map
+      ( \worryVal ->
+          let newWorryVal = operation worryVal `div` 3
+           in (test newWorryVal, newWorryVal)
+      )
+      items
 
 -- The critical insight here is that all arithmetic can be done modulo the
 -- product of all the moduli used for the monkeys' tests
-runMonkeyPart2 :: Int -> Monkey -> Vector (MonkeyId, WorryValue)
+runMonkeyPart2 :: Int -> Monkey -> Vector M.U (MonkeyId, WorryValue)
 runMonkeyPart2 modulo Monkey {items, operation, test} =
-  V.map
-    ( \worryVal ->
-        let newWorryVal = operation worryVal `mod` modulo
-         in (test newWorryVal, newWorryVal)
-    )
-    items
+  M.compute $
+    M.map
+      ( \worryVal ->
+          let newWorryVal = operation worryVal `mod` modulo
+           in (test newWorryVal, newWorryVal)
+      )
+      items
 
 giveItem :: WorryValue -> Monkey -> Monkey
-giveItem x Monkey {items, itemsInspected, ..} = Monkey {items = V.snoc items x, ..}
+giveItem x Monkey {items, itemsInspected, ..} = Monkey {items = M.compute $ M.snoc items x, ..}
 
 removeAllItems :: Monkey -> Monkey
-removeAllItems Monkey {..} = Monkey {items = V.empty, ..}
+removeAllItems Monkey {..} = Monkey {items = M.empty, ..}
 
 addItemsInspected :: Int -> Monkey -> Monkey
 addItemsInspected n Monkey {itemsInspected, ..} = Monkey {itemsInspected = itemsInspected + n, ..}
 
-adjust :: Int -> (a -> a) -> Vector a -> Vector a
-adjust idx f v = V.accumulate (\x _ -> f x) v (V.singleton (idx, ()))
+adjust :: M.Source r a => Int -> (a -> a) -> Vector r a -> Vector M.D a
+adjust idx f = M.imap (\i v -> if i == idx then f v else v)
 
-runRound :: (Monkey -> Vector (MonkeyId, WorryValue)) -> Vector Monkey -> Vector Monkey
-runRound runMonkey = go 0
+-- accumulate :: M.Source r a =>
+
+runRound :: (Monkey -> Vector M.U (MonkeyId, WorryValue)) -> Vector M.B Monkey -> Vector M.B Monkey
+runRound runMonkey = M.compute . go 0
   where
+    go :: MonkeyId -> Vector M.B Monkey -> Vector M.B Monkey
     go idx monkeys
-      | idx >= V.length monkeys = monkeys
+      | not $ M.isSafeIndex (M.size monkeys) idx = monkeys
       | otherwise =
-          let thrownItems = runMonkey (monkeys V.! idx)
-           in go
-                (idx + 1)
-                ( adjust idx (addItemsInspected (V.length thrownItems) . removeAllItems) $
-                    V.accumulate (flip giveItem) monkeys thrownItems
-                )
+          let thrownItems = runMonkey (M.evaluate' monkeys idx)
+           in monkeys
+                & adjust idx (addItemsInspected (M.unSz $ M.size thrownItems) . removeAllItems)
+                & accumulate (flip giveItem) thrownItems
+                & go (idx + 1)
+
+accumulate ::
+  (M.Index ix, M.Index ix', M.Source ixR (ix, b), M.Load srcR ix a, M.Manifest destR a) =>
+  (a -> b -> a) ->
+  M.Array ixR ix' (ix, b) ->
+  M.Array srcR ix a ->
+  M.Array destR ix a
+accumulate f targets src = M.withLoadMArrayST_ src $ \msrc ->
+  M.forM_ targets $ \(i, accVal) -> do
+    oldVal <- M.readM msrc i
+    M.write_ msrc i (f oldVal accVal)
 
 part1 :: ByteString -> Int
 part1 =
@@ -123,24 +135,21 @@ part1 =
     . take 2
     . reverse
     . sort
-    . V.toList
-    . V.map itemsInspected
+    . M.toList
+    . M.map itemsInspected
     . (!! 20)
     . iterate (runRound runMonkeyPart1)
     . readInput
 
 part2 :: ByteString -> Int
-part2 input = solution monkeys
+part2 input =
+  iterate' (runRound (runMonkeyPart2 modulo)) monkeys !! 10000
+    & M.map itemsInspected
+    & M.toList
+    & sort
+    & reverse
+    & take 2
+    & product
   where
-    solution =
-      product
-        . take 2
-        . reverse
-        . sort
-        . V.toList
-        . V.map itemsInspected
-        . (!! 10000)
-        . iterate' (runRound (runMonkeyPart2 modulo))
-
     monkeys = readInput input
-    modulo = product $ map testModulo $ V.toList monkeys
+    modulo = product $ map testModulo $ M.toList monkeys
